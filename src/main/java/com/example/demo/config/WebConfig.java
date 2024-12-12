a354132b90d4a62d7af96c8881c01f412d70aa25
shine26 <chews26@naver.com>
2024-12-12 16:27:13 +0900

    2. 인가에 대한 이해 : admin 인터셉터 적용

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.entity.Role;
 import com.example.demo.filter.AuthFilter;
 import com.example.demo.filter.RoleFilter;
+import com.example.demo.interceptor.AdminRoleInterceptor;
 import com.example.demo.interceptor.AuthInterceptor;
 import com.example.demo.interceptor.UserRoleInterceptor;
 import jakarta.servlet.Filter;
@@ -20,10 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     // TODO: 2. 인가에 대한 이해
     private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/admins/*", "/items/*"};
+    private static final String[] ADMIN_ROLE_REQUIRED_PATH_PATTERNS = {"/admin/*"};
     private static final String[] USER_ROLE_REQUIRED_PATH_PATTERNS = {"/reservations/*"};
 
     private final AuthInterceptor authInterceptor;
     private final UserRoleInterceptor userRoleInterceptor;
+    private final AdminRoleInterceptor adminRoleInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -31,26 +34,31 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns(AUTH_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE);
 
+        registry.addInterceptor(adminRoleInterceptor)
+                .addPathPatterns(ADMIN_ROLE_REQUIRED_PATH_PATTERNS)
+                .order(Ordered.HIGHEST_PRECEDENCE + 1);
+
         registry.addInterceptor(userRoleInterceptor)
                 .addPathPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE + 2);
     }
 
-    @Bean
-    public FilterRegistrationBean authFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new AuthFilter());
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        filterRegistrationBean.addUrlPatterns(AUTH_REQUIRED_PATH_PATTERNS);
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean userRoleFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new RoleFilter(Role.USER));
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 2);
-        filterRegistrationBean.addUrlPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS);
-        return filterRegistrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean authFilter() {
+//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new AuthFilter());
+//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        filterRegistrationBean.addUrlPatterns(AUTH_REQUIRED_PATH_PATTERNS);
+//        return filterRegistrationBean;
+//    }
+//
+//
+//    @Bean
+//    public FilterRegistrationBean userRoleFilter() {
+//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new RoleFilter(Role.USER));
+//        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 2);
+//        filterRegistrationBean.addUrlPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS);
+//        return filterRegistrationBean;
+//    }
 }
