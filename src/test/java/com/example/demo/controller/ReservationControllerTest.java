f4c1ea443faf89334243d342d828d9fcd3e08327
shine26 <chews26@naver.com>
2024-12-16 23:00:42 +0900

    9. 테스트 코드 : Controller Test 추가하기

--- src/test/java/com/example/demo/controller/ReservationControllerTest.java
+++ src/test/java/com/example/demo/controller/ReservationControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ReservationResponseDto;
 import com.example.demo.entity.Item;
 import com.example.demo.entity.Reservation;
 import com.example.demo.entity.ReservationStatus;
@@ -14,10 +15,16 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReservationController.class)
 class ReservationControllerTest {
@@ -32,10 +39,16 @@ class ReservationControllerTest {
     @DisplayName("예약 조회")
     void getAllReservations() throws Exception {
         // Given : 준비
-        ReservationStatus reservationStatus = ReservationStatus.PENDING;
-        LocalDateTime startAt = LocalDateTime.of(2020, 1, 1, 0, 0);
-        LocalDateTime endAt = LocalDateTime.of(2020, 1, 1, 0, 0);
-        Reservation reservation = new Reservation(1, 1, reservationStatus, startAt, endAt);
-        given(reservationService.getReservations()).willReturn(moc)
+        List<ReservationResponseDto> reservations = List.of(
+                new ReservationResponseDto(1L, "user1", "item1", LocalDateTime.of(2024, 12, 16, 9, 30), LocalDateTime.of(2020, 1, 1, 1, 0))
+        );
+        given(reservationService.getReservations()).willReturn(reservations);
+
+        // when : 액션
+        mockMvc.perform(get("/reservations"))
+                .andDo(print())
+                .andExpect(status().isOk()) // Then ; 결과 확인
+                .andExpect(jsonPath("$[0].nickname").value("user1"))
+                .andExpect(jsonPath("$[0].itemName").value("item1"));
     }
 }
