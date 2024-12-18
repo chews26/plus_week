204887e340ebdea93611ea2fe7052a4b813a19a0
shine26 <chews26@naver.com>
2024-12-18 23:02:27 +0900

    5. 동적 쿼리에 대한 이해 : 동적 쿼리가 제대로 작동하지 않던 문제 해결

--- src/main/java/com/example/demo/repository/ReservationRepositoryCustomImpl.java
+++ src/main/java/com/example/demo/repository/ReservationRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Reservation;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +16,13 @@ public class ReservationRepositoryCustomImpl implements ReservationRepositoryCus
 
     @Override
     public List<Reservation> searchReservations(Long userId, Long itemId) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (userId != null) {
+            builder.and(reservation.user.id.eq(userId));
+        }
+        if (itemId != null) {
+            builder.and(reservation.item.id.eq(itemId));
+        }
         return jpaQueryFactory
                 .selectFrom(reservation)
                 .leftJoin(reservation.user).fetchJoin()
